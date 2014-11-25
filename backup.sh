77d505c6e09ffda2c880dc5b84a15cfaff828091
daokhactuan48 <khactuank55@yahoo.com>
2014-11-25 16:50:37 +0700

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir /var/lib/backups
+mkdir /var/lib/backups/mysql
+backup_dir="/var/lib/backups/mysql"
+filename="${backup_dir}/mysql-`hostname`-`eval date +%Y%m%d-%H%M%S`.sql.gz"
+# Dump the entire MySQL database
+mysqldump --opt -u root -pWelcome123$ --all-databases | gzip > $filename
+# Delete backups older than 7 days
+find $backup_dir -ctime 7 -type f -delete
