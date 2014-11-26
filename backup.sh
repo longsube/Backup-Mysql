bfadcb0987f29c28f91c59b16c6f36db3985879b
daokhactuan48 <khactuank55@yahoo.com>
2014-11-26 15:41:14 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-mkdir /var/lib/backups
-mkdir /var/lib/backups/mysql
 backup_dir="/var/lib/backups/mysql"
 filename="${backup_dir}/mysql-`hostname`-`eval date +%Y%m%d-%H%M%S`.sql.gz"
 # Dump the entire MySQL database
