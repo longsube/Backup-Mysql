6f78a9e9483d0c7614430648ca25d9f9d5fcc657
daokhactuan48 <khactuank55@yahoo.com>
2014-11-27 16:58:49 +0700

    Update backupcomponent.sh

--- backupcomponent.sh
+++ backupcomponent.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 backup_dir="/var/lib/backups/mysql"
-filenamenova="${backup_dir}/mysql-nova-`hostname`-`eval date +%Y%m%d-%H%M%S`.sql.gz"
-filenamecinder="${backup_dir}/mysql-cinder-`hostname`-`eval date +%Y%m%d-%H%M%S`.sql.gz"
-filenameglance="${backup_dir}/mysql-glance-`hostname`-`eval date +%Y%m%d-%H%M%S`.sql.gz"
-filenamekeystone="${backup_dir}/mysql-keystone-`hostname`-`eval date +%Y%m%d-%H%M%S`.sql.gz"
-filenameneutron="${backup_dir}/mysql-neutron-`hostname`-`eval date +%Y%m%d-%H%M%S`.sql.gz"
-filenameinformation_schema="${backup_dir}/mysql-information_schema-`hostname`-`eval date +%Y%m%d-%H%M%S`.sql.gz"
-filenameperformance_schema="${backup_dir}/mysql-performance_schema-`hostname`-`eval date +%Y%m%d-%H%M%S`.sql.gz"
-filenamemysql="${backup_dir}/mysql-mysql-`hostname`-`eval date +%Y%m%d-%H%M%S`.sql.gz"
+filenamenova="${backup_dir}/mysql-nova-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
+filenamecinder="${backup_dir}/mysql-cinder-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
+filenameglance="${backup_dir}/mysql-glance-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
+filenamekeystone="${backup_dir}/mysql-keystone-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
+filenameneutron="${backup_dir}/mysql-neutron-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
+filenameinformation_schema="${backup_dir}/mysql-information_schema-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
+filenameperformance_schema="${backup_dir}/mysql-performance_schema-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
+filenamemysql="${backup_dir}/mysql-mysql-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
 # Dump the nova MySQL database
 mysqldump --opt -u root -pWelcome123 nova | gzip > $filenamenova
 # Dump the cinder MySQL database
