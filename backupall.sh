880f6ffe179a5560651e928c97146b9bbad6ccb4
daokhactuan48 <khactuank55@yahoo.com>
2014-11-27 17:02:02 +0700

    Update backupall.sh

--- backupall.sh
+++ backupall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 backup_dir="/var/lib/backups/mysql"
-filename="${backup_dir}/mysql-`hostname`-`eval date +%Y%m%d-%H%M%S`.sql.gz"
+filename="${backup_dir}/mysql-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
 # Dump the entire MySQL database
 mysqldump --opt -u root -pWelcome123$ --all-databases | gzip > $filename
 # Delete backups older than 7 days
