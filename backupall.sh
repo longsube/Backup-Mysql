0aab32ced5707b871178001d499fcca61926901d
longsube <longsube@gmail.com>
2014-12-02 08:48:21 +0700

    Sua folder backup

--- backupall.sh
+++ backupall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-backup_dir="/var/lib/backups/mysql"
+backup_dir="/root/backup/mysql"
 filename="${backup_dir}/mysql-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
 # Dump the entire MySQL database
 mysqldump --opt -u root --all-databases | gzip > $filename
